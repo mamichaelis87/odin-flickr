761743e148425b6ec7643d2ecfe639ce86b152ca
Mark Michaelis <mallenm1993@gmail.com>
2023-08-10 14:31:15 -0400

    Tweak controller to try and fix ENV variable issues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
     if params[:user_id]
-      flickr = Flickr.new("7c0c2bf5faaec563bbd5bd71f10229cd", "afbecd44ee7f6c23")
-      @photos = flickr.photos.search(api_key: "7c0c2bf5faaec563bbd5bd71f10229cd", user_id: params[:user_id])
-    else
-      @photos = []
+      flickr = Flickr.new(ENV["FLICKR_API_KEY"], ENV["FLICKR_SHARED_SECRET"])
+      @photos = flickr.photos.search(user_id: params[:user_id])
     end
   end
 end
